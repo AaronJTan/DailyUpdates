44d55d3308829e5e7aa3b023459c4dcde9da50df
Aaron Tan <aaronjtan00@gmail.com>
2023-11-03 23:49:45 -0400

    display date above logo on mobile devices

--- dailyupdates-ui/src/components/Navbar.js
+++ dailyupdates-ui/src/components/Navbar.js
@@ -8,19 +8,25 @@ export default function Navbar() {
     });
 
     return (
-        <div className="bg-[#3732b3] border-b-2">
-            <div className="container py-3 grid grid-cols-4 items-center">
-                <div className="hidden sm:block text-md text-[#bbc6ff]">{todaysDate}</div>
-
-                <h1
-                    className={`
-                         text-center tracking-tight 
-                         font-bold col-span-full sm:col-start-2 sm:col-span-2 text-white
-                    `}
-                >
-                    {appConfig.appName}
-                </h1>
+        <>
+            <div className="sm:hidden py-1 container bg-[#272397]">
+                <div className="sm:block text-md text-[#bbc6ff]">{todaysDate}</div>
             </div>
-        </div>
+
+            <div className="bg-[#3732b3] border-b-2">
+                <div className="container py-3 grid grid-cols-4 items-center">
+                    <div className="hidden sm:block text-md text-[#bbc6ff]">{todaysDate}</div>
+
+                    <h1
+                        className={`
+                            sm:text-center tracking-tight 
+                            font-bold col-span-full sm:col-start-2 sm:col-span-2 text-white
+                        `}
+                    >
+                        {appConfig.appName}
+                    </h1>
+                </div>
+            </div>
+        </>
     );
 }
