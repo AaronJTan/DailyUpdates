7c3d8e9bf0a0a32fe5f0dc21ed97ed836229b6de
Aaron Tan <aaronjtan00@gmail.com>
2023-12-18 13:26:09 -0500

    feat: cp24newsservice provide concrete method implementations for ArticleWebScraper abstract methods

--- DailyUpdates/src/main/java/com/aarontan/DailyUpdates/service/impl/CP24NewsServiceImpl.java
+++ DailyUpdates/src/main/java/com/aarontan/DailyUpdates/service/impl/CP24NewsServiceImpl.java
@@ -1,21 +1,16 @@
 package com.aarontan.DailyUpdates.service.impl;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import com.aarontan.DailyUpdates.exceptions.ConnectException;
 import com.aarontan.DailyUpdates.pojos.news.NewsArticleDetails;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import com.aarontan.DailyUpdates.service.ArticleWebScraper;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import com.aarontan.DailyUpdates.service.CP24NewsService;
 
 @Service
-public class CP24NewsServiceImpl implements CP24NewsService {
+public class CP24NewsServiceImpl extends ArticleWebScraper implements CP24NewsService {
 
     @Override
     public List<NewsArticleDetails> getLatestNews() {
@@ -27,21 +22,13 @@ public class CP24NewsServiceImpl implements CP24NewsService {
         return getArticles("https://www.cp24.com/world");
 	}
 
-    private List<NewsArticleDetails> getArticles(String pageUrl) {
-        try {
-            Document doc = Jsoup.connect(pageUrl).get();
-            Elements newsArticles = doc.select(".listInnerHorizontal .linklist li .teaserTitle");
-
-            return newsArticles.stream()
-                    .map(this::mapElementToArticle)
-                    .collect(Collectors.toList());
-
-        } catch (IOException e) {
-            throw new ConnectException("An error occurred when connecting to CP24");
-        }
+    @Override
+    protected String getNewsArticlesCSSSelectors() {
+        return ".listInnerHorizontal .linklist li .teaserTitle";
     }
-    
-    private NewsArticleDetails mapElementToArticle(Element article) {
+
+    @Override
+    protected NewsArticleDetails mapElementToArticle(Element article) {
         Element articleAnchorElem = article.getElementsByTag("a").first();
         String headline = articleAnchorElem.text();
         String url = articleAnchorElem.attr("href");
@@ -50,5 +37,10 @@ public class CP24NewsServiceImpl implements CP24NewsService {
                 .headline(headline)
                 .url(url)
                 .build();
+    }
+
+    @Override
+    protected String getConnectExceptionMessage() {
+        return "An error occurred when connecting to CP24";
     }
 }
