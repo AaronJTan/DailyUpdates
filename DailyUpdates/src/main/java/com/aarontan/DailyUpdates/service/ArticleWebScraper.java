70d90d1528fe331d1eda3ba8d4337b3c4a62517e
Aaron Tan <aaronjtan00@gmail.com>
2023-12-18 13:24:17 -0500

    feat: use template design pattern to web scrape news articles from different sources

---
+++ DailyUpdates/src/main/java/com/aarontan/DailyUpdates/service/ArticleWebScraper.java
@@ -0,0 +1,34 @@
+package com.aarontan.DailyUpdates.service;
+
+import com.aarontan.DailyUpdates.exceptions.ConnectException;
+import com.aarontan.DailyUpdates.pojos.news.NewsArticleDetails;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public abstract class ArticleWebScraper {
+    protected List<NewsArticleDetails> getArticles(String pageUrl) {
+        try {
+            Document doc = Jsoup.connect(pageUrl).get();
+            Elements newsArticles = doc.select(getNewsArticlesCSSSelectors());
+
+            return newsArticles.stream()
+                    .map(this::mapElementToArticle)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            throw new ConnectException(getConnectExceptionMessage());
+        }
+    }
+
+    protected abstract String getNewsArticlesCSSSelectors();
+    protected abstract NewsArticleDetails mapElementToArticle(Element article);
+    protected abstract String getConnectExceptionMessage();
+}
