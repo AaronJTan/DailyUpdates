581ff29a07c53ecbc6a11345f5866f365946480c
Aaron Tan <aaronjtan00@gmail.com>
2023-12-20 15:29:17 -0500

    feat: update top sources table every 24 hours

--- DailyUpdates/src/main/java/com/aarontan/DailyUpdates/service/impl/NewsAPIServiceImpl.java
+++ DailyUpdates/src/main/java/com/aarontan/DailyUpdates/service/impl/NewsAPIServiceImpl.java
@@ -3,6 +3,7 @@ package com.aarontan.DailyUpdates.service.impl;
 import com.aarontan.DailyUpdates.models.TopSource;
 import com.aarontan.DailyUpdates.payload.response.NewsAPIorg.ArticleResponse;
 import com.aarontan.DailyUpdates.payload.response.NewsAPIorg.SourceResponse;
+import com.aarontan.DailyUpdates.pojos.news.NewsAPIorg.SourceAPIModel;
 import com.aarontan.DailyUpdates.repository.*;
 import com.aarontan.DailyUpdates.service.NewsAPIService;
 import com.aarontan.DailyUpdates.service.SourceMapper;
@@ -10,10 +11,12 @@ import com.aarontan.DailyUpdates.utils.UriParamsBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.*;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,23 +24,15 @@ import java.util.Map;
 public class NewsAPIServiceImpl implements NewsAPIService {
     private final RestTemplate restTemplate;
     private final SourceRepository sourceRepository;
-    private final CategoryRepository categoryRepository;
-    private final LanguageRepository languageRepository;
-    private final CountryRepository countryRepository;
     private final SourceMapper sourceMapper;
-
 
     @Value("${NewsAPIKey}")
     private String newsAPIKey;
 
-    public NewsAPIServiceImpl(RestTemplate restTemplate, SourceRepository sourceRepository,
-                              CategoryRepository categoryRepository, LanguageRepository languageRepository,
-                              CountryRepository countryRepository, SourceMapper sourceMapper) {
+    public NewsAPIServiceImpl(RestTemplate restTemplate, SourceRepository sourceRepository
+            , SourceMapper sourceMapper) {
         this.restTemplate = restTemplate;
         this.sourceRepository = sourceRepository;
-        this.categoryRepository = categoryRepository;
-        this.languageRepository = languageRepository;
-        this.countryRepository = countryRepository;
         this.sourceMapper = sourceMapper;
     }
 
@@ -46,32 +41,29 @@ public class NewsAPIServiceImpl implements NewsAPIService {
                 .fromHttpUrl("https://newsapi.org/v2");
     }
 
-//    @Override
-//    public SourceResponse getTopSources(Map<String, String> queryParams) {
-//        HttpEntity<String> httpEntity = createHttpEntityWithHeaders();
-//
-//        UriComponentsBuilder uriComponentsBuilder = initUriComponentsBuilder()
-//                .path("/top-headlines/sources");
-//
-//        final String[] validQueryParams = new String[]{"category", "language", "country"};
-//        UriParamsBuilder uriParamsBuilder = new UriParamsBuilder(uriComponentsBuilder, validQueryParams, queryParams);
-//        uriComponentsBuilder = uriParamsBuilder.addValidQueryParamsToUriComponentsBuilder();
-//
-//        SourceResponse sourceResponse = restTemplate.exchange(uriComponentsBuilder.build().toUri(),
-//                HttpMethod.GET,
-//                httpEntity,
-//                SourceResponse.class).getBody();
-//
-//        List<TopSource> sourceList = new ArrayList<>();
-//
-//        for (Source source: sourceResponse.getSources()) {
-//            TopSource topSource = sourceMapper.convertToTopSource(source);
-//            sourceList.add(topSource);
-//        }
-//        sourceRepository.saveAll(sourceList);
-//
-//        return sourceResponse;
-//    }
+    @Scheduled(fixedRate = 86400000)
+    public void updateTopSources() {
+        System.out.println("Updating database with top sources");
+        HttpEntity<String> httpEntity = createHttpEntityWithHeaders();
+
+        UriComponentsBuilder uriComponentsBuilder = initUriComponentsBuilder()
+                .path("/top-headlines/sources");
+
+        SourceResponse sourceResponse = restTemplate.exchange(uriComponentsBuilder.build().toUri(),
+                HttpMethod.GET,
+                httpEntity,
+                SourceResponse.class).getBody();
+
+        List<TopSource> sourceList = new ArrayList<>();
+
+        for (SourceAPIModel source: sourceResponse.getSourceAPIModels()) {
+            TopSource topSource = sourceMapper.convertToTopSource(source);
+            sourceList.add(topSource);
+        }
+        sourceRepository.saveAll(sourceList);
+
+        System.out.println("Source update complete.");
+    }
 
     @Override
     public SourceResponse getTopSources(Map<String, String> queryParams) {
